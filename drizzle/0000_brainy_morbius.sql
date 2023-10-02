2ac76d5f1c1cec1da7943b3894edea7092fa05a7
Mohamed Sami <srmf2025@gmail.com>
2023-10-02 12:45:39 +0530

    chore: setup miration script

--- drizzle/0000_brainy_morbius.sql
+++ drizzle/0000_brainy_morbius.sql
@@ -7,6 +7,15 @@ CREATE TABLE IF NOT EXISTS "tasks" (
 	"user_email" text
 );
 --> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "users" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"name" text NOT NULL,
+	"email" text NOT NULL,
+	"password" text NOT NULL,
+	"created_at" timestamp DEFAULT now(),
+	CONSTRAINT "users_email_unique" UNIQUE("email")
+);
+--> statement-breakpoint
 DO $$ BEGIN
  ALTER TABLE "tasks" ADD CONSTRAINT "tasks_user_email_users_email_fk" FOREIGN KEY ("user_email") REFERENCES "users"("email") ON DELETE no action ON UPDATE no action;
 EXCEPTION
